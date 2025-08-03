96a521f630bbbf4296ce1232f3f9441ba747d21a
zahirul <zahirulopel@gmail.com>
2025-08-03 12:56:56 +0600

    add solution for both challenge 2 and 3

--- exercises/closures.js
+++ exercises/closures.js
@@ -5,4 +5,22 @@ function createFunction() {
   };
 }
 
-export { createFunction };
+// Challenge 2: Function Printer
+function createFunctionPrinter(input) {
+  return function () {
+    console.log(input);
+  };
+}
+
+//Challenge 3: Independent Counters
+function outer() {
+  let counter = 0;
+  function incrementCounter() {
+    counter++;
+    console.log("counter", counter);
+    return counter; // this code has added to test
+  }
+  return incrementCounter;
+}
+
+export { createFunction, createFunctionPrinter, outer };
--- exercises/closures.test.js
+++ exercises/closures.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect, beforeEach, vitest, afterEach } from "vitest";
-import { createFunction } from "./closures";
+import { createFunction, createFunctionPrinter, outer } from "./closures";
 
 describe("Challenge 1", () => {
   let logSpy;
@@ -15,5 +15,35 @@ describe("Challenge 1", () => {
     expect(logSpy).toHaveBeenCalled();
     expect(logSpy).toBeCalledWith("hello");
     expect(logSpy).toBeCalledTimes(1);
+  });
+});
+
+describe("Challenge 2", () => {
+  let logSpy;
+  beforeEach(() => {
+    logSpy = vitest.spyOn(console, "log");
+  });
+  afterEach(() => {
+    logSpy.mockRestore();
+  });
+  test("print input text", () => {
+    const printFunc = createFunctionPrinter("input");
+    printFunc();
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toBeCalledWith("input");
+    expect(logSpy).toBeCalledTimes(1);
+  });
+});
+
+describe("Challenge 3", () => {
+  test("counter has different value when call mulitple times", () => {
+    const counterA = outer();
+    const counterB = outer();
+
+    counterA(); // ➞ "counter 1"
+    counterA(); // ➞ "counter 2"
+
+    expect(counterA()).toBe(3);
+    expect(counterB()).toBe(1);
   });
 });
