0566149a3475aa9c90fa80f3f3f34092b7cdab90
zahirul <zahirulopel@gmail.com>
2025-08-10 13:08:16 +0600

    add solution upto exercise 4 of async

---
+++ exercises/async.js
@@ -0,0 +1,38 @@
+// Challenge 1: Understanding Execution Order
+function testMe() {
+  setTimeout(() => {
+    console.log("Howdy");
+  }, 0);
+  console.log("Partnah");
+}
+
+// Challenge 2: Delayed Greeting
+function delayedGreet() {
+  setTimeout(() => {
+    console.log("welcome");
+  }, 3000);
+}
+
+// Challenge 3: Say Hello, Then Goodbye
+function helloGoodBye() {
+  setTimeout(() => {
+    console.log("good bye");
+  }, 2000);
+  console.log("hello");
+}
+
+// Challenge 4: Repeating Logs
+function brokenRecord() {
+  setInterval(() => {
+    console.log("hi again");
+  }, 1000);
+}
+
+// Challenge 5: Limited Repeats
+function limitedRepeat() {
+  const id = setInterval(() => {
+    console.log("hi for now");
+  }, 1000);
+  setTimeout(() => clearInterval(id), 5001);
+}
+limitedRepeat();
---
+++ exercises/async.test.js
@@ -0,0 +1,22 @@
+import { afterEach, beforeEach, describe, expect, test, vitest } from "vitest";
+import { testMe } from "./async";
+
+describe("challenge 1", () => {
+  let logSpy;
+  beforeEach(() => {
+    vitest.useFakeTimers();
+    logSpy = vitest.spyOn(console, "log");
+    vitest.spyOn(global, "setTimeout");
+  });
+  afterEach(() => {
+    vitest.useRealTimers();
+    logSpy.mockRestore();
+  });
+
+  test("Log Partnah first then Howdy", () => {
+    testMe();
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 0);
+  });
+});
